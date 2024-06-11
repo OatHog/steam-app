5ee5fd56641bbe45e3630d057bd907f9152c944d
OatHog <joshua.jason665@gmail.com>
2024-06-11 19:36:14 +0200

    SWR refetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import useSWR, { mutate } from "swr";
-import PlayerCard from "./components/PlayerCard";
-import { PlayerSummary } from "@/types/Player";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import useIntervalFetch from "./hooks/useSetInterval";
 import PlayerDataDisplay from "./components/PlayerDataDisplay";
+import useSWR from "swr";
 
 export default function Home() {
-  const url = useMemo(() => "/api/playersummaries", []);
-  const { data, error, isLoading } = useIntervalFetch(url, 300000)
+  const fetcher = (url: string) => fetch(url).then((r) => r.json());
+  const { data, error, isLoading } = useSWR("/api/playersummaries", fetcher, {
+    refreshInterval: 300000,
+  });
 
   return <PlayerDataDisplay data={data} error={error} isLoading={isLoading} />;
 }
