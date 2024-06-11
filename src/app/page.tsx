20f2e3a8631ba4cb5ef1a21e7d91a726d34d8e99
OatHog <joshua.jason665@gmail.com>
2024-06-11 19:41:44 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import useSWR from "swr";
 export default function Home() {
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
   const { data, error, isLoading } = useSWR("/api/playersummaries", fetcher, {
-    refreshInterval: 5000,
+    refreshInterval: 300000,
   });
 
   return <PlayerDataDisplay data={data} error={error} isLoading={isLoading} />;
