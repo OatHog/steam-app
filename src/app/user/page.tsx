b4794e3763451dace4a60cc0ee32bb4ab7672025
OatHog <joshua.jason665@gmail.com>
2024-06-11 13:21:14 +0200

    Wrapped in suspense

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -10,36 +10,44 @@ import { IoArrowBack } from "react-icons/io5";
 type Props = {};
 
 export default function Page({}: Props) {
-  const searchParams = useSearchParams();
-  const steamId = searchParams.get("steamid");
-  const fetcher = (url: RequestInfo) => fetch(url).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(
-    `/api/recentlyplayedgames?steamid=${steamId}`,
-    fetcher
-  );
+  // Wrap the part using useSearchParams
+  const Content = () => {
+    const searchParams = useSearchParams();
+    const steamId = searchParams.get("steamid");
 
-  if (!steamId) {
-    redirect("/");
-  }
+    const fetcher = (url: RequestInfo) => fetch(url).then((res) => res.json());
+    const { data, error, isLoading } = useSWR(
+      `/api/recentlyplayedgames?steamid=${steamId}`,
+      fetcher
+    );
 
-  if (error)
+    if (!steamId) {
+      redirect("/");
+    }
+
+    if (error) return <div>Failed to load data: {error.message}</div>;
+    if (isLoading) return <div>Loading...</div>;
+    if (!data || !data.response || !data.response.games)
+      return <div>No games found.</div>;
+
     return (
-      <Suspense>
-        <div>Failed to load data: {error.message}</div>
-      </Suspense>
+      <>
+        <div className="flex flex-col items-center m-24 mt-12 text-white text-xl font-bold">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {data.response.games.map((game: SteamGame) => (
+              <GameTile
+                key={game.appid}
+                gameName={game.name}
+                gameIconUrl={game.img_icon_url}
+                totalPlayTime={game.playtime_forever}
+                twoWeekPlayTime={game.playtime_2weeks}
+              />
+            ))}
+          </div>
+        </div>
+      </>
     );
-  if (isLoading)
-    return (
-      <Suspense>
-        <div>Loading...</div>
-      </Suspense>
-    );
-  if (!data || !data.response || !data.response.games)
-    return (
-      <Suspense>
-        <div>No games found.</div>
-      </Suspense>
-    );
+  };
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
@@ -50,20 +58,8 @@ export default function Page({}: Props) {
         <IoArrowBack className="text-white text-2xl mr-2" />
         Return
       </Link>
-
-      <div className="flex flex-col items-center m-24 mt-12 text-white text-xl font-bold">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {data.response.games.map((game: SteamGame) => (
-            <GameTile
-              key={game.appid}
-              gameName={game.name}
-              gameIconUrl={game.img_icon_url}
-              totalPlayTime={game.playtime_forever}
-              twoWeekPlayTime={game.playtime_2weeks}
-            />
-          ))}
-        </div>
-      </div>
+      {/* Render the content wrapped in Suspense */}
+      <Content />
     </Suspense>
   );
 }
